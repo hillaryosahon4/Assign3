79a47724e40831aa7dcc21561a9f5884fac04133
Osahon <hillary.osahon4@mail.dcu.ie>
2019-12-15 23:12:16 +0000

    Add a welcome text and image to Welcome tab.

--- SDA_Assign3_2019/app/src/main/java/com/example/sdaassign32019johndoe/Welcome.java
+++ SDA_Assign3_2019/app/src/main/java/com/example/sdaassign32019johndoe/Welcome.java
@@ -1,12 +1,31 @@
 package com.example.sdaassign32019johndoe;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
 
 
 public class Welcome extends Fragment {
 
     public Welcome(){
+    }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState){
+
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        return  root;
     }
 
 }
