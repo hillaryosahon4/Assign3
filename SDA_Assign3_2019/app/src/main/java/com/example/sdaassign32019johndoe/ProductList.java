4300f5828cf705cdb8c5b3a4abb348825abfcf3b
Osahon <hillary.osahon4@mail.dcu.ie>
2019-12-17 07:49:05 +0000

    Changed mFlavor to mShirt arraylist in ProductList class

--- SDA_Assign3_2019/app/src/main/java/com/example/sdaassign32019johndoe/ProductList.java
+++ SDA_Assign3_2019/app/src/main/java/com/example/sdaassign32019johndoe/ProductList.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 public class ProductList extends Fragment {
 
     private static final String TAG = "RecyclerViewActivity";
-    private ArrayList<FlavorAdapter> mFlavor = new ArrayList<>();
+    private ArrayList<FlavorAdapter> mShirt = new ArrayList<>();
 
     public ProductList() {
         // Required empty public constructor
@@ -35,21 +35,21 @@ public class ProductList extends Fragment {
         View root = inflater.inflate(R.layout.fragment_product_list, container, false);
         // Create an ArrayList of T-Shirts objects
 
-        mFlavor.add(new FlavorAdapter("Beat shirt", "Spring 2020", R.drawable.t_shirt_4));
-        mFlavor.add(new FlavorAdapter("Eclair shirt", "Summer 2020 ", R.drawable.t_shirt_5));
-        mFlavor.add(new FlavorAdapter("Froyo shirt", "Summer 2019", R.drawable.t_shirt_9));
-        mFlavor.add(new FlavorAdapter("Ginger shirt", "Summer 2019", R.drawable.t_shirt_8));
-        mFlavor.add(new FlavorAdapter("Honey shirt", "Spring 2019", R.drawable.t_shirt_4));
-        mFlavor.add(new FlavorAdapter("Ice shirt", "Spring 2020", R.drawable.t_shirt_1));
-        mFlavor.add(new FlavorAdapter("Jelly shirt", "Spring 2019", R.drawable.t_shirt_2));
-        mFlavor.add(new FlavorAdapter("KitKat shirt ", "Winter 2020", R.drawable.t_shirt_3));
-        mFlavor.add(new FlavorAdapter("Lollipop shirt", "Summer 2020", R.drawable.t_shirt_6));
-        mFlavor.add(new FlavorAdapter("Mars shirt", "Spring 2020", R.drawable.t_shirt_7));
+        mShirt.add(new FlavorAdapter("Beat shirt", "Spring 2020", R.drawable.t_shirt_4));
+        mShirt.add(new FlavorAdapter("Eclair shirt", "Summer 2020 ", R.drawable.t_shirt_5));
+        mShirt.add(new FlavorAdapter("Froyo shirt", "Summer 2019", R.drawable.t_shirt_9));
+        mShirt.add(new FlavorAdapter("Ginger shirt", "Summer 2019", R.drawable.t_shirt_8));
+        mShirt.add(new FlavorAdapter("Honey shirt", "Spring 2019", R.drawable.t_shirt_4));
+        mShirt.add(new FlavorAdapter("Ice shirt", "Spring 2020", R.drawable.t_shirt_1));
+        mShirt.add(new FlavorAdapter("Jelly shirt", "Spring 2019", R.drawable.t_shirt_2));
+        mShirt.add(new FlavorAdapter("KitKat shirt ", "Winter 2020", R.drawable.t_shirt_3));
+        mShirt.add(new FlavorAdapter("Lollipop shirt", "Summer 2020", R.drawable.t_shirt_6));
+        mShirt.add(new FlavorAdapter("Mars shirt", "Spring 2020", R.drawable.t_shirt_7));
 
         //start it with the view
         Log.d(TAG, "Starting recycler view");
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView_view);
-        FlavorViewAdapter recyclerViewAdapter = new FlavorViewAdapter(getContext(), mFlavor);
+        FlavorViewAdapter recyclerViewAdapter = new FlavorViewAdapter(getContext(), mShirt);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
